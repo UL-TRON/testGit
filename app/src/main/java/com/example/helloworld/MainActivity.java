0890397e3f530ebba4fae16ce8b6b2b59716cf99
BigLazybones <1933323175@qq.com>
2021-01-18 16:29:34 +0800

    修改并提交到分支

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MainActivity","onCreate execute");
+        Log.d("MainActivity","onCreate");
     }
 }
